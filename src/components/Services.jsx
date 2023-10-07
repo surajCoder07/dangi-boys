9e3be5fe881799278e1bdbb2e78fc8e34a09d50b
Suraj Singh <surajsinghdangi07@gmail.com>
2023-10-07 15:53:30 +0530

    minor changes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,35 +1,30 @@
-import React from "react";
-import { services } from "../utils/services";
+import React from 'react'
+import { services } from '../utils/services'
 
 const Services = () => {
   return (
-    <div id="services" className="p-web max-sm:p-mobile py-10 bg-primary-bg">
-      <h1 className="text-primary-orange text-xl heading ">Our Services </h1>
+    <div id='services' className='p-web max-sm:p-mobile py-10 bg-primary-bg'>
+    
+        <h1 className='text-primary-orange text-xl heading '>Our Services </h1>
 
-      <div className="py-10 flex flex-wrap justify-around">
-        {services?.map((service) => {
-          return (
-            <div
-              key={service.id}
-              className="card p-3 text-white w-[350px] gap-3 z-30 my-5 flex flex-col "
-            >
-              <img
-                src={service?.src}
-                alt="img"
-                className="z-40 h-[200px] max-sm:h-[150px] w-full object-cover rounded-t-md"
-              />
-              <h1 className="text-white text-center py-3 z-50 font-semibold text-2xl max-sm:text-xl">
-                {service?.title}
-              </h1>
-              <p className="text-gray-300 text-justify px-2  max-sm:text-sm">
-                {service?.desc}
-              </p>
-            </div>
-          );
-        })}
+      <div className='py-10 flex flex-wrap justify-around'>
+
+      {
+        services?.map(service=>{
+          return <div key={service.id} className='card p-3 text-white w-[350px] gap-3 z-30 my-5 flex flex-col '>
+          <img src={service?.src} alt='img' className='z-40 h-[200px] max-sm:h-[150px] w-full object-cover rounded-t-md' />
+          <h1 className='text-white text-center py-3 z-50 font-semibold text-2xl max-sm:text-xl'>{service?.title}</h1>
+          <p className='text-gray-300 text-justify px-2  max-sm:text-sm'>
+          {service?.desc}
+          </p>
+        </div>
+        })
+      }
+
+        
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Services;
+export default Services
